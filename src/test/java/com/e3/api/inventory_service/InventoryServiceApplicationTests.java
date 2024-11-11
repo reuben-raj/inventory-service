45c3cf7ad1242dd5091f6b2fb9735c84d19e2fa8
reuben-raj <reuben.raj90@gmail.com>
2024-11-11 17:50:33 +0800

    add base structure and configurations

--- src/test/java/com/e3/api/inventory_service/InventoryServiceApplicationTests.java
+++ src/test/java/com/e3/api/inventory_service/InventoryServiceApplicationTests.java
@@ -2,12 +2,21 @@ package com.e3.api.inventory_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("unit")
 class InventoryServiceApplicationTests {
 
+	// @Test
+	// void contextLoads() {
+	// }
+
 	@Test
-	void contextLoads() {
+	void testMain() {
+		System.setProperty("spring.profiles.active", "unit");
+
+		InventoryServiceApplication.main(new String[] {});
 	}
 
 }
